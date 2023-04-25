26746803bf448a9c50a5eccde88f88c45d94d023
Zia Rahman <7999789+xiarahman@users.noreply.github.com>
2023-04-26 03:52:22 +0500

    Added working example code

---
+++ src/Home.tsx
@@ -0,0 +1,118 @@
+import {
+  Breadcrumb,
+  Layout,
+  Menu,
+  PageHeader,
+  Button,
+  Divider,
+  Checkbox,
+  Form,
+  Input,
+} from "antd";
+
+import styled from "styled-components";
+
+// Styled component named StyledButton
+const StyledButton = styled(Button)`
+  background-color: black;
+  color: white;
+`;
+
+const { Header, Content, Footer } = Layout;
+
+const items = [
+  { label: "item 1", key: "item-1" }, // remember to pass the key prop
+  { label: "item 2", key: "item-2" }, // which is required
+  {
+    label: "sub menu",
+    key: "submenu",
+    children: [{ label: "item 3", key: "submenu-item-1" }],
+  },
+];
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+export const Home = () => {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <PageHeader
+            className="site-page-header"
+            onBack={() => null}
+            title="Title"
+            subTitle="This is a subtitle"
+          />
+          <Divider />
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <StyledButton>Button</StyledButton>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
